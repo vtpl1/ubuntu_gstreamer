7c71f8d731045beb52354438d85a4b88b5da68a4
Monotosh Das <monotosh.das@videonetics.com>
2020-12-14 07:00:11 +0000

    Bump version: 0.0.0 → 0.0.1.dev0

--- src/vtpl_config.h
+++ src/vtpl_config.h
@@ -1,4 +1,4 @@
-#define VTPL_VERSION "0.0.0.prod0"
+#define VTPL_VERSION "0.0.1"
 #define VTPL_DESCRIPTION "Communicate with Vtpl Vms"
 #define VTPL_LICENSE "Proprietary"
 #define VTPL_PACKAGE_NAME "Vtpl VMS plugins"
